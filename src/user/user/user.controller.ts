1483e2bfdf8beb40b501b249e4617de53f8d6a74
Renaldi710 <renaldizaki7@gmail.com>
2026-07-17 01:08:37 +0700

    nestjs dasar v1.1

---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Get, Req } from '@nestjs/common';
+import type { Request } from 'express';
+
+@Controller('/api/users')
+export class UserController {
+  @Get('/:id')
+  getById(@Req() request: Request): string {
+    return `GET ${request.params.id}`;
+  }
+
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'Hello NestJS';
+  }
+}
